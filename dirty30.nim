00a9d6fc2edeba93933f3ef74b236c977feac28c
nervecenter <nervecenter7@gmail.com>
2022-11-11 16:56:26 -0500

    Added recursive implementation to Nim as an option.

--- dirty30.nim
+++ dirty30.nim
@@ -4,6 +4,7 @@ import tables
 import sugar
 import strformat
 import strutils
+import sequtils
 import algorithm
 
 randomize()
@@ -25,7 +26,7 @@ proc ascii_histogram(rolls: seq[int], max_bar_length=20) =
             bar_str = repeat('+', int(max_bar_length_float * bar_proportion))
         echo &"{k:5d} {freq:5d} {proportion:0.1f}% \t{bar_str}"
 
-proc dirty30(num_dice: int): int =
+proc dirty30_imperative(num_dice: int): int =
     var
         num_rolls = 0
         num_dice_remaining = num_dice
@@ -40,7 +41,24 @@ proc dirty30(num_dice: int): int =
 
     return num_rolls
 
+proc rolld6(): int =
+    return rand(1..6)
+
+proc rollnd6(n: int): seq[int] =
+    return collect(newSeq):
+        for _ in 0..<n: rolld6()
+
+proc dirty30_rec(num_dice: int, num_rolls: int): int =
+    if num_dice == 0:
+        return num_rolls
+    else:
+        let new_num_dice = rollnd6(num_dice).filter(r => r < 6).len
+        return dirty30_rec(new_num_dice, num_rolls + 1)
+
+proc dirty30_recursive(num_dice: int): int =
+    dirty30_rec(num_dice, 0)
+
 let rolls = collect(newSeq):
-    for _ in 0..100000: dirty30(30)
+    for _ in 0..100000: dirty30_imperative(30)
 
 ascii_histogram(rolls, 40)
